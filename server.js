cbe28af0d5597a3f6d9702a9e115441d6985dbb9
samanthaallen <allen.c.samantha@gmail.com>
2020-07-19 16:13:28 -0700

    Adding Mongoose

--- server.js
+++ server.js
@@ -1,28 +1,39 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// Connect to DB
+connectDB();
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
+
 const app = express();
 
 // Dev logging middleware
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // Mount routers
-app.use('/api/v1/bootcamps', bootcamps);
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     `Server runing in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
   )
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server + exit process
+  server.close(() => process.exit(1));
+});
